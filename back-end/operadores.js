d92676d0de8e52f261fe136583b542ad7aef9ac5
Belchiorlsa <anaisabelyribeiro43@gmail.com>
2024-02-29 11:16:55 -0400

    first commit

---
+++ back-end/operadores.js
@@ -0,0 +1,48 @@
+
+//1
+//const nome = ' Ana isabely ';
+//const idade = 18;
+//console.log("Olá meu nome é",nome, "e eu tenho",idade, "anos!");
+
+//2
+ //let exemplo= "letras";
+ //let maiusculas=
+ //exemplo.toUpperCase();
+ //console.log(maiusculas);
+
+ //let exemplo= "letras";
+ //let minuscula= exemplo.toLowerCase();
+ // console.log(minuscula);
+
+//3
+ //let variavel=45;
+//console.log (typeof variavel);
+
+//4 
+/*let peso = 58;
+let altura= 1.60;
+let imc= (peso/(altura**2));
+console.log (imc);*/
+
+//5
+/*let vr1=8;
+let vr2= 9;
+console.log (vr1 + vr2);
+console.log (vr1 - vr2);
+console.log (vr1 * vr2);
+console.log (vr1 / vr2);*/
+
+//6
+/*let idade1= 56
+let idade2= 25
+console.log(idade1<idade2);*/
+
+//7
+/*let idade= 45;
+let conferir= idade>=18 ? "maior de idade": "menor de idade";
+console.log(conferir);?*/
+
+//8 
+/*let num= 6;
+let confe= num % 2 ==0? "par" : "impar";
+console.log(confe);*/
